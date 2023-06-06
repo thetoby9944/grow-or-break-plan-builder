e0049482e4b132ced84aa6464d45a6a09dfbfa04
toby9944 <thetoby@web.de>
2023-06-06 12:53:07 +0200

    Fix

--- main.py
+++ main.py
@@ -862,7 +862,7 @@ with export_plan_container:
             value_vars=result_plan.columns,
             value_name="Sets",
             ignore_index=False,
-        ).reset_index(inplace=True, drop=False).groupby("Block").apply(lambda x: x.to_json(orient='records')).values
+        ).reset_index(drop=False).groupby("Block").apply(lambda x: x.to_json(orient='records')).values
     )
 
 
