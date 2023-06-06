82bdb67142e6531f887c0c6d1277a705241b21f3
toby9944 <thetoby@web.de>
2023-06-06 12:47:09 +0200

    Fix

--- main.py
+++ main.py
@@ -862,7 +862,7 @@ with export_plan_container:
             value_vars=result_plan.columns,
             value_name="Sets",
             ignore_index=False,
-        ).groupby("Block").apply(lambda a: a.drop("Block", axis=1)[:])
+        )#.groupby("Block").apply(lambda a: a.drop("Block", axis=1)[:])
     )
 
 
